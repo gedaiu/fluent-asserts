f39bd9b49d105da701b8684ddbcfa77765e56c4b
Szabo Bogdan <bogdan.szabo@altom.ro>
2017-01-02 18:07:06 +0200

    fixed json message

--- source/http/json.d
+++ source/http/json.d
@@ -1,11 +1,13 @@
 module http.json;
 
+import std.exception, std. conv;
+
 import vibe.data.json;
 
-string[] keys(Json obj) {
+string[] keys(Json obj, const string file = __FILE__, const size_t line = __LINE__) {
   string[] list;
 
-  assert(obj.type == Json.Type.object, "The object should be an object.");
+  enforce(obj.type == Json.Type.object, "The json should be an object. `" ~ obj.type.to!string ~ "` found.", file, line);
 
   foreach(string key, Json value; obj) {
     list ~= key;
