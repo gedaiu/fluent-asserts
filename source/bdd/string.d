54099bcc0ac4fc5145fde0ff0f37749ea87fd475
Szabo Bogdan <bogdan.szabo@altom.ro>
2017-02-03 06:00:29 +0200

    Added start with and end with for string

--- source/bdd/string.d
+++ source/bdd/string.d
@@ -53,6 +53,110 @@ struct ShouldString {
 
     result(isPresent, strVal ~ (isPresent ? " is present" : " is not present") ~ " in `" ~ testData ~"`.", file, line);
   }
+
+  void startWith(T)(const T someString, const string file = __FILE__, const size_t line = __LINE__) {
+    auto strVal = "`" ~ someString.to!string ~ "`";
+
+    addMessage("start with");
+    addMessage(strVal);
+    beginCheck;
+
+    auto index = testData.indexOf(someString);
+    auto doesStartWith = index == 0;
+
+    result(doesStartWith, "`" ~ testData ~ "`" ~ (doesStartWith ? " does start with " : " does not start with ") ~ strVal, file, line);
+  }
+
+  void endWith(T)(const T someString, const string file = __FILE__, const size_t line = __LINE__) {
+    auto strVal = "`" ~ someString.to!string ~ "`";
+
+    addMessage("end with");
+    addMessage(strVal);
+    beginCheck;
+
+    auto index = testData.lastIndexOf(someString);
+
+    static if(is(T == string)) {
+      auto doesStartWith = index == testData.length - someString.length;
+    } else {
+      auto doesStartWith = index == testData.length - 1;
+    }
+
+    result(doesStartWith, "`" ~ testData ~ "`" ~ (doesStartWith ? " does end with " : " does not end with ") ~ strVal, file, line);
+  }
+}
+
+@("string startWith")
+unittest {
+  should.not.throwAnyException({
+    "test string".should.startWith("test");
+  });
+
+  should.throwException!TestException({
+    "test string".should.startWith("other");
+  }).msg.should.contain("`test string` does not start with `other`");
+
+  should.not.throwAnyException({
+    "test string".should.not.startWith("other");
+  });
+
+  should.throwException!TestException({
+    "test string".should.not.startWith("test");
+  }).msg.should.contain("`test string` does start with `test`");
+
+
+
+  should.not.throwAnyException({
+    "test string".should.startWith('t');
+  });
+
+  should.throwException!TestException({
+    "test string".should.startWith('o');
+  }).msg.should.contain("`test string` does not start with `o`");
+
+  should.not.throwAnyException({
+    "test string".should.not.startWith('o');
+  });
+
+  should.throwException!TestException({
+    "test string".should.not.startWith('t');
+  }).msg.should.contain("`test string` does start with `t`");
+}
+
+@("string endWith")
+unittest {
+  should.not.throwAnyException({
+    "test string".should.endWith("string");
+  });
+
+  should.throwException!TestException({
+    "test string".should.endWith("other");
+  }).msg.should.contain("`test string` does not end with `other`");
+
+  should.not.throwAnyException({
+    "test string".should.not.endWith("other");
+  });
+
+  should.throwException!TestException({
+    "test string".should.not.endWith("string");
+  }).msg.should.contain("`test string` does end with `string`");
+
+
+  should.not.throwAnyException({
+    "test string".should.endWith('g');
+  });
+
+  should.throwException!TestException({
+    "test string".should.endWith('t');
+  }).msg.should.contain("`test string` does not end with `t`");
+
+  should.not.throwAnyException({
+    "test string".should.not.endWith('w');
+  });
+
+  should.throwException!TestException({
+    "test string".should.not.endWith('g');
+  }).msg.should.contain("`test string` does end with `g`");
 }
 
 @("string contain")
