46b6f381facb15ba94f26f3f6b6ae3de21d88b2e
Szabo Bogdan <bogdan.szabo@altom.ro>
2017-04-19 01:03:03 +0300

    skip unit-threaded on ldc 1.0.0

--- travis-ci.sh
+++ travis-ci.sh
@@ -6,8 +6,13 @@ set -e -x -o pipefail
 dub build --combined -b release --compiler=$DC
 dub clean --all-packages
 
+# run unit tests
 dub test :core --compiler=$DC
 dub test :vibe --compiler=$DC
 
 # run a build for unit-threaded
-dub --root=test/unit-threaded --compiler=$DC
+echo 'Run unit-threaded build:' $UNIT_THREADED
+
+if [[ $UNIT_THREADED ]]; then
+  dub --root=test/unit-threaded --compiler=$DC
+fi
