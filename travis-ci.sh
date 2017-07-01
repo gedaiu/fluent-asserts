e03759b7b270fb2852bcc9a6a54d7571334017e6
Szabo Bogdan <bogdan.szabo@altom.ro>
2017-07-01 23:06:07 +0300

    fix 32 bit build

--- travis-ci.sh
+++ travis-ci.sh
@@ -4,7 +4,7 @@ set -e -x -o pipefail
 
 # test for successful 32-bit build
 if [ "$DC" == "dmd" ]; then
-	dub build --combined --arch=x86 --config
+	dub build --combined --arch=x86
 	dub clean --all-packages
 fi
 
