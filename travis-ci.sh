a0a858ccfcb3acc66ee4b9cea894f0f968a2a839
Szabo Bogdan <bogdan.szabo@altom.ro>
2017-07-01 23:02:37 +0300

    fix 32 bit build

--- travis-ci.sh
+++ travis-ci.sh
@@ -2,17 +2,24 @@
 
 set -e -x -o pipefail
 
+
+# test for successful release build
+dub build --combined -b release --compiler=$DC --config=${VIBED_DRIVER=libevent}
+dub clean --all-packages
+
+# test for successful 32-bit build
+if [ "$DC" == "dmd" ]; then
+	dub build --combined --arch=x86 --config=${VIBED_DRIVER=libevent}
+	dub clean --all-packages
+fi
+
 # test for successful release build
 dub build --combined -b release --compiler=$DC
 dub clean --all-packages
 
 # run unit tests for 64 bit build
-dub test :core --compiler=$DC --arch=x86_64
-dub test :vibe --compiler=$DC --arch=x86_64
-
-# run unit tests for 32 bit build
-dub test :core --compiler=$DC --arch=x86
-dub test :vibe --compiler=$DC --arch=x86
+dub test :core --compiler=$DC
+dub test :vibe --compiler=$DC
 
 # run a build for unit-threaded
 if ! $($DC --version | grep -q 1.0.0); then
