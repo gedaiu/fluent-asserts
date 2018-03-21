238c6453627d2bd0890f40b330100c479d7bebe3
Szabo Bogdan <szabobogdan@yahoo.com>
2018-03-21 15:53:49 +0200

    update the vibe json file

--- travis-ci.sh
+++ travis-ci.sh
@@ -30,7 +30,7 @@ dub --root=test/disabledDiffResult --compiler=$DC --arch=x86_64
 
 # run a build for vibe-d 0.8
 if ! $($DC --version | grep -q '2.073\|2.074\|2.075\|2.076\|2.077\|2.078\|2.079'); then
-  dub --root=test/vibe-0.7 --compiler=$DC --arch=x86_64
+  dub --root=test/vibe-0.8 --compiler=$DC --arch=x86_64
 fi
 
 # run a build for vibe-d 0.7
