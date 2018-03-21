d228489189e5f7293d89fbe70a61a048ca554827
Szabo Bogdan <szabobogdan@yahoo.com>
2018-03-22 01:36:57 +0200

    update travis script

--- travis-ci.sh
+++ travis-ci.sh
@@ -32,7 +32,7 @@ dc_version=$("$DC" --version | perl -pe '($_)=/([0-9]+([.][0-9]+)+)/')
 # run a build for vibe-d 0.8
 if [[ ${DC=dmd} = dmd ]]; then
   if [[ "$dc_version" =~ ^("2.073.2"|"2.077.0"|"2.078.0"|"2.079.0")$ ]]; then
-    dub --root=test/vibe-0.7 --compiler=$DC --arch=x86_64
+    dub --root=test/vibe-0.8 --compiler=$DC --arch=x86_64
   else
     echo "We don't compile vibe-d 0.8 with dmd" $dc_version
   fi
