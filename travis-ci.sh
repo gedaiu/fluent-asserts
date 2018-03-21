b16b1c06d35ab65031ef7fecdb094e9b82dfd2e3
Szabo Bogdan <szabobogdan@yahoo.com>
2018-03-22 00:07:13 +0200

    update travis script

--- travis-ci.sh
+++ travis-ci.sh
@@ -2,6 +2,8 @@
 
 set -e -x -o pipefail
 
+export DC=dmd
+
 # test for successful 32-bit build
 if [ "$DC" == "dmd" ]; then
 	dub build --combined --arch=x86
@@ -27,13 +29,22 @@ dub --root=test/disabledMessageResult --compiler=$DC --arch=x86_64
 # run a build for DisableDiffResult
 dub --root=test/disabledDiffResult --compiler=$DC --arch=x86_64
 
+dc_version=$("$DC" --version | perl -pe '($_)=/([0-9]+([.][0-9]+)+)/')
 
 # run a build for vibe-d 0.8
-if ! $($DC --version | grep -q '2.073\|2.074\|2.075\|2.076\|2.077\|2.078\|2.079'); then
-  dub --root=test/vibe-0.8 --compiler=$DC --arch=x86_64
+if [[ ${DC=dmd} = dmd ]]; then
+  if [[ "$dc_version" =~ ^("2.073.2"|"2.077.0"|"2.078.0"|"2.079.0")$ ]]; then
+    dub --root=test/vibe-0.7 --compiler=$DC --arch=x86_64
+  else
+    echo "We don't compile vibe-d 0.8 with dmd" $dc_version
+  fi
 fi
 
 # run a build for vibe-d 0.7
-if ! $($DC --version | grep -q 2.073); then
-  dub --root=test/vibe-0.7 --compiler=$DC --arch=x86_64
+if [[ ${DC=dmd} = dmd ]]; then
+  if [[ ${dc_version} = *"2.073"* ]]; then
+    dub --root=test/vibe-0.7 --compiler=$DC --arch=x86_64
+  else
+    echo "We don't compile vibe-d 0.7 with dmd" $dc_version
+  fi
 fi
