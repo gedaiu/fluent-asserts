9244e469ebbeec6cbaee8862f9682a549024e3e4
Szabo Bogdan <szabobogdan@yahoo.com>
2018-06-24 02:23:05 +0200

    public import vibe packages when htey are available

--- source/fluent/asserts.d
+++ source/fluent/asserts.d
@@ -1,3 +1,11 @@
 module fluent.asserts;
 
 public import fluentasserts.core.base;
+
+version(Have_vibe_d_data) {
+  public import fluentasserts.vibe.json;
+}
+
+version(Have_vibe_d_data) {
+  public import fluentasserts.vibe.request;
+}
