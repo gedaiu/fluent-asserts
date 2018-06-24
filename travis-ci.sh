aac42a4dac48cc489c2a3a69970da247f8bc5ccb
Szabo Bogdan <szabobogdan@yahoo.com>
2018-06-24 11:16:14 +0200

    verbose test

--- travis-ci.sh
+++ travis-ci.sh
@@ -22,23 +22,23 @@ cd ..
 ./trial/trial --compiler=$DC
 
 # run a build for unit-threaded
-dub --root=test/unit-threaded --compiler=$DC --arch=x86_64
+dub -v --root=test/unit-threaded --compiler=$DC --arch=x86_64
 
 # run a build for DisableSourceResult
-dub --root=test/disabledSourceResult --compiler=$DC --arch=x86_64
+dub -v --root=test/disabledSourceResult --compiler=$DC --arch=x86_64
 
 # run a build for DisableMessageResult
-dub --root=test/disabledMessageResult --compiler=$DC --arch=x86_64
+dub -v --root=test/disabledMessageResult --compiler=$DC --arch=x86_64
 
 # run a build for DisableDiffResult
-dub --root=test/disabledDiffResult --compiler=$DC --arch=x86_64
+dub -v --root=test/disabledDiffResult --compiler=$DC --arch=x86_64
 
 dc_version=$("$DC" --version | perl -pe '($_)=/([0-9]+([.][0-9]+)+)/')
 
 # run a build for vibe-d 0.8
 if [[ ${DC=dmd} = dmd ]]; then
   if [[ "$dc_version" =~ ^("2.073.2"|"2.077.0"|"2.078.0"|"2.079.0")$ ]]; then
-    dub --root=test/vibe-0.8 --compiler=$DC --arch=x86_64
+    dub -v --root=test/vibe-0.8 --compiler=$DC --arch=x86_64
   else
     echo "We don't compile vibe-d 0.8 with dmd" $dc_version
   fi
@@ -47,7 +47,7 @@ fi
 # run a build for vibe-d 0.7
 if [[ ${DC=dmd} = dmd ]]; then
   if [[ ${dc_version} = *"2.073"* ]]; then
-    dub --root=test/vibe-0.7 --compiler=$DC --arch=x86_64
+    dub -v --root=test/vibe-0.7 --compiler=$DC --arch=x86_64
   else
     echo "We don't compile vibe-d 0.7 with dmd" $dc_version
   fi
