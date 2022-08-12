9f19d6fcb39690374bb4f2cf8fd1710434042b15
Szabo Bogdan <szabobogdan@yahoo.com>
2022-08-12 10:12:09 +0200

    remove circle ci

--- .circleci/other.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e -x -o pipefail
-
-dub --root=test/unit-threaded
-
-dub --root=test/disabledSourceResult
-dub --root=test/disabledMessageResult
-dub --root=test/disabledDiffResult
